a08eadaa40f452a0e11f79ccb793c829926139de
Kamal Kumar <iamkamalkumar17@gmail.com>
2023-08-15 17:35:49 +0530

    completed

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB").then(console.log("mongodb c
 const userSchema =new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -87,11 +88,28 @@ app.get("/register", (req, res)=> {
     res.render("register")
 });
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}).then((foundUsers)=> {
+        if(foundUsers) {
+            res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+    }).catch((err)=>{console.log(err)});
+
+});
+
+app.get("/submit", function(req, res) {
     if(req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+    //passport store user in req
+    User.findById(req.user.id).then((foundUser) => {
+        foundUser.secret=submittedSecret;
+        foundUser.save().then(res.redirect("/secrets"));
+    }).catch((err)=>{console.log(err)});
 })
 
 app.post("/register", (req, res) => {
